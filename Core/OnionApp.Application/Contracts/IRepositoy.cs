c0439ce8d4f786854721e31a7e549bd7d95aa84b
Münevver Orman <munevverorman1@gmail.com>
2026-01-23 22:14:13 +0300

    Add project files.

---
+++ Core/OnionApp.Application/Contracts/IRepositoy.cs
@@ -0,0 +1,17 @@
+﻿namespace OnionApp.Application.Contracts
+{
+    public interface IRepositoy<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task CreateAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+
+    }
+}
---
+++ Infasructure/OnionApp.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OnionApp.Domain.Entities;
+
+namespace OnionApp.Persistence.Context
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+
+
+
+}
+
